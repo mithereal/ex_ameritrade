b960c784def13cab4d0a1a5db858ada4f065d25b
mithereal <mithereal@gmail.com>
2021-08-26 19:24:35 +0000

    add package

--- mix.exs
+++ mix.exs
@@ -6,8 +6,13 @@ defmodule Ameritrade.MixProject do
       app: :ameritrade,
       version: "1.0.0",
       elixir: "~> 1.12",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "Ameritrade",
+      source_url: "https://github.com/mithereal/ex_ameritrade.git"
     ]
   end
 
@@ -22,7 +27,23 @@ defmodule Ameritrade.MixProject do
   defp deps do
     [
       {:jason, "~> 1.0"},
-      {:oauth2, "~> 1.0 or ~> 2.0"}
+      {:oauth2, "~> 1.0 or ~> 2.0"},
+      {:inch_ex, ">= 0.0.0", only: [:test, :dev]}
+    ]
+  end
+
+  defp description() do
+    "A TD Ameritrade Client for Elixir"
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "ameritrade",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/mithereal/ex_ameritrade"}
     ]
   end
 end
