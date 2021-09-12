2cdc1ae8747a1fe4f660aee5bfb9e0da01acb69c
mithereal <mithereal@gmail.com>
2021-09-12 23:41:09 +0000

    add docs

--- mix.exs
+++ mix.exs
@@ -1,18 +1,22 @@
 defmodule Ameritrade.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/mithereal/ex_ameritrade.git"
+  @version "1.1.0"
+
   def project do
     [
       app: :ameritrade,
-      version: "1.1.0",
-      elixir: "~> 1.12",
+      version: @version,
+      elixir: "~> 1.9",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
       deps: deps(),
+      docs: docs(),
       name: "Ameritrade",
-      source_url: "https://github.com/mithereal/ex_ameritrade.git"
+      source_url: @source_url
     ]
   end
 
@@ -45,6 +49,17 @@ defmodule Ameritrade.MixProject do
       files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => "https://github.com/mithereal/ex_ameritrade"}
+    ]
+  end
+
+  defp docs() do
+    [
+      main: "readme",
+      name: "Ameritrade",
+      source_ref: "v#{@version}",
+      canonical: "http://hexdocs.pm/ameritrade",
+      source_url: @source_url,
+      extras: ["README.md", "LICENSE"]
     ]
   end
 end
