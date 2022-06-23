4241f5a2f4dce55ebaa012fa5709aff220efdaf1
mithereal <mithereal@gmail.com>
2022-06-22 21:09:44 -0700

    check formatting and credo

--- mix.exs
+++ mix.exs
@@ -32,7 +32,6 @@ defmodule Ameritrade.MixProject do
     [
       {:jason, "~> 1.0"},
       {:oauth2, "~> 1.0 or ~> 2.0"},
-      {:inch_ex, ">= 0.0.0", only: [:test, :dev]},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.6.1", only: [:dev, :test], runtime: false}
     ]
